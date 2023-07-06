6b88f5150a438b1d00e7a56028167057c4a1c736
Diego Alonso Molina <diegusnueva@gmail.com>
2023-07-06 19:32:52 +0200

    Update enviar_formulario.php

--- enviar_formulario.php
+++ enviar_formulario.php
@@ -2,12 +2,14 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
+  $telefono = $_POST['numero']; // Obtener el número de teléfono del formulario
   $mensaje = $_POST['mensaje'];
-  
+
   // Construir el cuerpo del correo electrónico
   $asunto = 'Nuevo mensaje de contacto';
   $contenido = "Nombre: $nombre\n";
   $contenido .= "Correo electrónico: $correo\n";
+  $contenido .= "Teléfono: $telefono\n"; // Agregar el número de teléfono al contenido del mensaje
   $contenido .= "Mensaje: $mensaje\n";
   
   // Enviar el correo electrónico
